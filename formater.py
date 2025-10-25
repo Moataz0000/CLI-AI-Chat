a68d3ba2e21c182a599f0ac145882fced5169a11
Moataz Fawzy <142129253+Moataz0000@users.noreply.github.com>
2025-10-25 15:28:36 +0300

    Refactor clear_last_lines to improve readability

--- formater.py
+++ formater.py
@@ -4,6 +4,6 @@ import sys
 def clear_last_lines(n=1):
     """Utility: move cursor up and clear n lines."""
     for _ in range(n):
-        sys.stdout.write("\033[F")      # move cursor up
-        sys.stdout.write("\033[K")      # clear line
+        sys.stdout.write("\033[F")     
+        sys.stdout.write("\033[K")      
     sys.stdout.flush()
